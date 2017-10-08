c011a2ba7ad8cad4225c587490d4411e0c8260b9
mnshaw <mnshaw@andrew.cmu.edu>
2017-10-08 17:47:11 -0400

    Finish cart

--- scripts/todo.js
+++ scripts/todo.js
@@ -8,3 +8,25 @@ function findTotal(){
   }
   document.getElementById('total').textContent = tot;
 }
+
+
+function remove(elem){
+  className = elem.className;
+  var arr = document.getElementsByClassName(className);
+  for (var i = 0; i < arr.length; i++) {
+    arr[i].style.display="none";
+  }
+  tot = document.getElementById('total').textContent;
+  if (tot > 0) {
+    document.getElementById('total').textContent = tot - 1;
+  }
+
+  price = document.getElementById('totalPrice').textContent;
+  if (className == "original") {
+    price = price.slice(1) - 18;
+  } else {
+    price = price.slice(1) - 12;
+  }
+
+  document.getElementById('totalPrice').textContent = "$"+price;
+}
